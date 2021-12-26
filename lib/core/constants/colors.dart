ed7bcf56002fdc08820c285029d415a0fcb08cf7
its-faatimas <ozodakhon2000@gmail.com>
2021-12-26 19:53:22 +0500

    Initial commit

---
+++ lib/core/constants/colors.dart
@@ -0,0 +1,15 @@
+import 'dart:ui';
+
+class ConstColors {
+  static const Color  black = Color(0xFF000000);
+  static const Color  green = Color(0xFF7C9A92);
+  static const Color  whiteSeventy = Color(0xFFFFFFFF);
+  static const Color  grey = Color(0xFFBEC2C2);
+  static const Color  creamColor = Color(0xFFF7F3F0);
+  static const Color textMeditation = Color(0xFF253334);
+  static const Color statsColour = Color(0xFF95CBCF);
+  static const Color noStatColour = Color(0xFF3A5051);
+  static const Color showAllColor = Color(0xFFC4C4C4);
+  static const Color sleepColor = Color(0xFF69B09C);
+  static const Color deepColor = Color(0xFF498A78);
+}
